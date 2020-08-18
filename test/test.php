36d1049042d4ceee7ea7e5dfaea50dc1bf0e115f
Edson Magombe <edson.patricio.39@gmail.com>
2020-08-18 10:33:32 +0200

    improved test feature

--- test/test.php
+++ test/test.php
@@ -4,23 +4,8 @@ require_once __DIR__."/../autoload.php";
 
 use emagombe\DataTable;
 
-$host = "localhost";
-$port = 3306;
-$database = "mk";
-$username = "root";
-$password = "";
-$charset = "utf8";
 
-$conn = new PDO("mysql:host=$host;dbname=$database;port=$port;charset=$charset", $username, $password);
-
-$sql = "SELECT * FROM gener;";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-
-$result = [];
-foreach($stmt as $item) {
-	$result[] = $item;
-}
+$result = json_decode(file_get_contents(__DIR__."/data.json"), true);
 
 DataTable::create($result)->addColumn("action", function($row) {
 	return "<b>".$row["id"]."</b>";
